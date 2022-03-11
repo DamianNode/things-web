c2bafd336b13254aaeef649d332ef02682e381f5
Damian Janus <damian.janus@gmail.com>
2022-03-11 21:15:17 +0100

    generate next.js projects, clean up, add custom css resets

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Thing Web Clone</title>
+        <meta name="description" content="Web app version of popular to-do app named Things" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
